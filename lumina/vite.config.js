d6bc856f89c3cf542563f85226f6032f3504b13d
andreguigo <andreguigo@gmail.com>
2025-09-18 16:37:00 -0300

    tornando em PWA

--- lumina/vite.config.js
+++ lumina/vite.config.js
@@ -3,12 +3,41 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    VitePWA({
+      registerType: 'autoUpdate', // 'autoUpdate' ou 'prompt'
+      injectRegister: 'auto',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,jpg,jpeg,gif,woff,woff2,ttf,eot}'],
+      },
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        name: 'Lumina Devocionais',
+        short_name: 'Lumina',
+        description: 'Receba uma palavra de fé sob medida para sua alma, a cada acesso.',
+        theme_color: '#ec6349',
+        icons: [
+          {
+            src: 'img/icons/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'img/icons/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
@@ -16,3 +45,4 @@ export default defineConfig({
     },
   },
 })
+
