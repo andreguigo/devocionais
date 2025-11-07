6c4e00fcf8935dc618e2774fccde25fa32137ce3
andreguigo <andreguigo@gmail.com>
2025-11-06 23:37:16 -0300

    adicionando diversas melhorias

--- lumina/dev-dist/sw.js
+++ lumina/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ab4816ec'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.44jl8fpd1cg"
+    "revision": "0.e4p7920hr4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
